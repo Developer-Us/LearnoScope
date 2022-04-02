533a34ebbbeb7b85f4dc99ed8822d94caca8a118
Vedant Kulkarni <vedantkulkarni22@gmail.com>
2022-04-02 17:08:35 +0530

    Started making video screen

---
+++ src/Components/VideoWatchSection.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../Styles/VideoWatchSection.css';
+
+export default function VideoWatchSection() {
+  return (
+  <>
+      {/* <div className="VideoWatchSection">
+        <div className="VideoPlaySection">
+          <div className="VideoPlayer">
+            <div className="Video">
+              <video className="VideoTag" src="Sample Video/sampleVideo.mov" type="video/mp4" controls></video>
+            </div>
+          </div>
+          <div className='VideoMetaData'>
+            <div className="VideoTitle">
+              Piyush Bansal's Lenskart's Journey - Shark Tank India | Madhukrishna Nipankar overtook piyush | Yadhnesh starts laughing
+            </div>
+
+          </div>
+
+          <div className="ChannelInfo">
+            <div className="ChannelPic"><img src="Images/lenskart.jpg" style={{ borderRadius: "50%" }} height="50px" width="50px" alt=".." /></div>
+            <div className="ChannelName">Lenskart</div>
+          </div>
+        </div>
+
+        <div className="QuizSection">
+          Quiz Section
+        </div>
+      </div>     */}
+  </>
+  )
+}
