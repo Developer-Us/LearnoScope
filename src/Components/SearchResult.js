d86cae99190bbb04adc1fce9d66370ace698e3ec
amit sali <amitsaliap@gmail.com>
2022-04-02 18:36:14 +0530

    minor touch ups

---
+++ src/Components/SearchResult.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import SearchResultVideoContainer from './SearchResultVideoContainer'
+// import '../styles/SearchResult.css';
+
+export default function SearchResult() {
+  return (
+   <>
+
+
+
+
+ 
+
+  <SearchResultVideoContainer/>
+  <SearchResultVideoContainer/>
+  <SearchResultVideoContainer/>
+  <SearchResultVideoContainer/>
+
+  
+
+  
+   
+   </>
+
+  )
+}
