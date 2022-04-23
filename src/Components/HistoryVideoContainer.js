866e6b7d8c35e3a20f4aceec7791f7a2f56ff517
amit sali <amitsaliap@gmail.com>
2022-04-23 15:31:38 +0530

    history component complete

--- src/Components/HistoryVideoContainer.js
+++ src/Components/HistoryVideoContainer.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function HistoryVideoContainer() {
   return (
-    <div>HistoryVideoContainer</div>
-  )
+    <>
+       <div class=" mx-3 card mb-3" styles="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="Images/hill.jpg" class="card-img" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
